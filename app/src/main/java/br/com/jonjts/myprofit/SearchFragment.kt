941e9af008d89010852fbd65da2212f62b9222ff
Jonas <jonjts@gmail.com>
2018-12-19 09:09:00 -0300

    Ajustes de layout na tela inicial

--- app/src/main/java/br/com/jonjts/myprofit/SearchFragment.kt
+++ app/src/main/java/br/com/jonjts/myprofit/SearchFragment.kt
@@ -14,15 +14,6 @@ import androidx.fragment.app.Fragment
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SearchFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class SearchFragment : Fragment() {
 
 
